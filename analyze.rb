a92471e003f3c312a054207ab7a499dd9c418cad
Jordan Feldstein <jfeldstein@gmail.com>
2018-07-29 12:56:25 -0700

    Skin Tone analysis

--- analyze.rb
+++ analyze.rb
@@ -16,6 +16,7 @@ puts "Seeing #{users.length} users"
 
 reaction_totals = {}
 user_favorites = []
+skin_tones = [0, 0, 0, 0, 0, 0]
 
 users.each do |user|
   history = client.reactions_list(
@@ -40,6 +41,13 @@ users.each do |user|
     puts reactions.map(&:name).join(', ')
 
     reactions.map(&:name).each do |reaction|
+      reaction, skin_tone_str = reaction.split('::')
+
+      if skin_tone_str
+        puts skin_tone_str[-1].inspect
+        skin_tones[1+skin_tone_str[-1].to_i] += 1
+      end
+
       reaction_totals[reaction] ||= {}
       reaction_totals[reaction][user.name] ||= 0
       reaction_totals[reaction][user.name] += 1
@@ -62,7 +70,9 @@ reaction_totals.each do |emoji, counts|
     total: counts.values.inject(0, :+),
   }
 end
-popular.sort_by! {|react| react[:total] }
+popular.sort_by! { |react| react[:total] }
+
+user_favorites.sort_by! { |user_fav| user_fav[0] }
 
 puts
 puts
@@ -70,3 +80,5 @@ puts
 puts "*User Favorites*", user_favorites.map{|f| f[2].nil? ? nil : "@#{f[0]}: :#{f[1]}: (Used x#{f[2]})"}.compact
 puts
 puts "*10 most popular reactions*", popular.last(10).reverse.map{|r| ":#{r[:name]}: (Used x#{r[:total]})"}
+puts
+puts "*Skin Tone Popularity*", skin_tones.map.with_index{|t, s| ":wave::skin-tone-#{s+1}: x#{t}" }
