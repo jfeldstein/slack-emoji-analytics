6aea188d4e671c1b684b1fe99e170679bdd22cf5
Jordan Feldstein <jfeldstein@gmail.com>
2018-07-29 13:15:57 -0700

    Fix bugs in sorting

--- analyze.rb
+++ analyze.rb
@@ -19,13 +19,20 @@ user_favorites = []
 skin_tones = [0, 0, 0, 0, 0, 0]
 
 users.each do |user|
+  if user.deleted
+    puts "Skipping #{user.name} (deleted)"
+    next
+  end
+
+  puts "Fetching #{user.name}"
+
   history = client.reactions_list(
     user: user.id,
     full: true,
-    count: 1000
+    count: 1000,
+    oldest: 28.days.ago
   )
 
-  puts user.name
   user_totals = {}
 
   items = history.items.map{|item| [item.message, item.comment, item.file].compact }.flatten
@@ -44,8 +51,7 @@ users.each do |user|
       reaction, skin_tone_str = reaction.split('::')
 
       if skin_tone_str
-        puts skin_tone_str[-1].inspect
-        skin_tones[1+skin_tone_str[-1].to_i] += 1
+        skin_tones[(skin_tone_str.last.to_i)-1] += 1
       end
 
       reaction_totals[reaction] ||= {}
@@ -57,7 +63,7 @@ users.each do |user|
     end
   end
 
-  user_favorites << [user.name, user_totals.sort.first].flatten
+  user_favorites << [user.name, user_totals.sort_by{ |_e, c| c }.last(3).reverse].flatten
 
   puts
 end
@@ -68,6 +74,7 @@ reaction_totals.each do |emoji, counts|
   popular << {
     name: emoji,
     total: counts.values.inject(0, :+),
+    people: counts.keys.length,
   }
 end
 popular.sort_by! { |react| react[:total] }
@@ -77,8 +84,8 @@ user_favorites.sort_by! { |user_fav| user_fav[0] }
 puts
 puts
 
-puts "*User Favorites*", user_favorites.map{|f| f[2].nil? ? nil : "@#{f[0]}: :#{f[1]}: (Used x#{f[2]})"}.compact
+puts "*User Favorites*", user_favorites.map{|f| f[2].nil? ? nil : "@#{f[0]}: :#{f[1]}:x#{f[2]}, :#{f[3]}:x#{f[4]}, :#{f[5]}:x#{f[6]}"}.compact
 puts
-puts "*10 most popular reactions*", popular.last(10).reverse.map{|r| ":#{r[:name]}: (Used x#{r[:total]})"}
+puts "*10 most popular reactions*", popular.last(10).reverse.map{|r| ":#{r[:name]}: (Used x#{r[:total]} by #{r[:people]} people)"}
 puts
 puts "*Skin Tone Popularity*", skin_tones.map.with_index{|t, s| ":wave::skin-tone-#{s+1}: x#{t}" }
